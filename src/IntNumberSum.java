af205fe104d9f77d4cf7ad6782eecb680edf1aa6
alhatmii <alhatmii.150@gmail.com>
2023-01-09 10:42:13 +0400

    The code 1st push

---
+++ src/IntNumberSum.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IntNumberSum {
+	
+	static Scanner sc = new Scanner (System.in);
+	
+	public static List<int[]> findpairs(int[] numbers, int targetSum){
+		List<int[]> pairs = new ArrayList<>();
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length; j++) {
+				if (numbers[i] + numbers[j] == targetSum) {
+					pairs.add(new int [] { numbers[i], numbers[j]});
+				}
+			}
+		}
+		return pairs;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+		System.out.println("Enter the required Sum that is not bigger than 10:");
+		int targetSum = sc.nextInt();
+		System.out.println("\n");
+
+		List<int[]> pairs = findpairs(numbers, targetSum);
+		for (int[] pair : pairs) {
+			System.out.println(pair[0] + "," + pair[1]);
+		}
+		
+	}
+
+}
